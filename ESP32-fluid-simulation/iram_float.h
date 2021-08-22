407811901b45e3eadb43924e4754688f62eb6b05
Kenny Peng <knypng44@gmail.com>
2021-08-22 15:49:57 -0700

    Drop defines in iram_float.h and fix documentation

--- ESP32-fluid-simulation/iram_float.h
+++ ESP32-fluid-simulation/iram_float.h
@@ -1,21 +1,17 @@
-// A very initial implementation of an integer that must be represented as a float, 
-//  implemented with the volatile keyword to block optimizations. The result is forcing 
-//  the compiler to use the l32i/s32i instructions thus allowing iram_float_t objects to 
-//  be stored in IRAM, all while still using high level code
+// iram_float_t
+// A float that must be stored in IRAM as an integer. The result is forcing the compiler 
+//  to use the l32i/s32i instructions (using other instructions cause a LoadStoreError).
 // Original issue (and assembly solution): https://github.com/espressif/esp-idf/issues/3036
-// This will probably be spun off into a separate library in the future.
 
 #ifndef IRAM_FLOAT_H
 #define IRAM_FLOAT_H
 
 #include <cstdint>
 
-#define BIT_REPR(x) (*reinterpret_cast<volatile uint32_t*>(&(x)))
-#define BIT_INTERPR(x) (*reinterpret_cast<float*>(&(x)))
-
 class iram_float_t{
     public:
-        iram_float_t(float value = 0) : _value(BIT_REPR(value)) {}
+        iram_float_t(float value = 0) 
+            : _value(*reinterpret_cast<volatile uint32_t*>(&value)) {}
 
         #ifdef ESP32
         void* operator new[] (size_t size){ // Forces allocation from IRAM
@@ -25,7 +21,7 @@ class iram_float_t{
 
         operator float() const {
             uint32_t a_raw = _value;
-            return BIT_INTERPR(a_raw);
+            return *reinterpret_cast<float*>(&a_raw);
         }
     private:
         volatile uint32_t _value;
