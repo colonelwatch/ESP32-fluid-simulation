2f542532813b187928a208ddd09f2e530ad893c1
Kenny Peng <53552802+colonelwatch@users.noreply.github.com>
2023-09-23 08:22:08 -0700

    Use percent density error figure

--- test/animate.py
+++ test/animate.py
@@ -39,7 +39,10 @@ def read_field_file(file_path, type):
 velocity_frames = read_field_file('sim_velocity.txt', 'vector')
 pressure_frames = read_field_file('sim_pressure.txt', 'scalar')
 color_frames = read_field_file('sim_color.txt', 'scalar')
+
+# see ESP32-fluid-simulation for how and why percent density error is constructed
 divergence_frames = np.abs(read_field_file('sim_divergence.txt', 'scalar'))
+pct_rho_err_frames = np.array([100*DT*divergence_frame for divergence_frame in divergence_frames])
 
 frame_interval = 1000//FRAMERATE
 frame_count = velocity_frames.shape[0]
@@ -47,14 +50,14 @@ frame_count = velocity_frames.shape[0]
 fig, (ax1, ax2, ax3) = plt.subplots(1, 3)
 ax1.set_title('Pressure and Velocity')
 ax2.set_title('Color')
-ax3.set_title('Absolute Divergence (Bad!)')
+ax3.set_title('Pct density error')
 
 artists = []
 foo = np.random.random(size=(N, N))
 artists.append(ax1.quiver(foo, foo, scale=100, scale_units='inches', color='blue'))
 artists.append(ax1.imshow(foo, cmap='hot', interpolation='nearest', vmin=-2, vmax=2, animated=True))
 artists.append(ax2.imshow(foo, interpolation='nearest', vmin=0, vmax=1, animated=True))
-artists.append(ax3.imshow(foo, cmap='hot', interpolation='nearest', vmin=0, vmax=1, animated=True))
+artists.append(ax3.imshow(foo, cmap='hot', interpolation='nearest', vmin=0, vmax=100, animated=True))
 
 def update(i):
     # convert from i-j to ordinary Cartesian (x-y)
@@ -63,12 +66,12 @@ def update(i):
 
     pressure_frame = pressure_frames[i, :, :]
     color_frame = color_frames[i, :, :]
-    divergence_frame = divergence_frames[i, :, :]
+    pct_rho_err_frame = pct_rho_err_frames[i, :, :]
 
     artists[0].set_UVC(u, v)
     artists[1].set_array(pressure_frame)
     artists[2].set_array(color_frame)
-    artists[3].set_array(divergence_frame)
+    artists[3].set_array(pct_rho_err_frame)
 
     return artists
 
